ab2cfbc60b09f3189a023330784eaee116b5726d
SERGIO TEJEDOR MORENO <119494713+sertemo@users.noreply.github.com>
2023-05-20 15:52:35 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -208,9 +208,10 @@ if __name__ == '__main__':
             st.metric("Predicción",pred)
             st.metric("Confianza:",conf)
 
-            actualizar_historico_predicciones(model_name,uploaded_file,pred,conf)
-            with st.expander("Ver histórico de predicciones"):
-                plotear_historico_predicciones()
+            actualizar_historico_predicciones(model_name,uploaded_file,pred,conf)    
+
+    with st.expander("Ver histórico de predicciones"):
+        plotear_historico_predicciones()
 
             
 
